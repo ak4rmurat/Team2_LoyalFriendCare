51fe709d57b9298344baa590358943eebceaafd9
murat akar <akar.murat@hotmail.com>
2024-05-15 22:39:30 +0300

    Murat/15.05.2024/ Homepage footer bolumundeki logoların ve admin dashboard locateleri eklendi

--- src/test/java/pages/AdminDashboardPage.java
+++ src/test/java/pages/AdminDashboardPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -9,6 +11,80 @@ public class AdminDashboardPage {
 
         PageFactory.initElements(Driver.getDriver(),this);
 
-
     }
+
+    @FindBy(xpath = "//a[normalize-space()='admin01']")
+    public WebElement adminLoginButton; // Header / admin01 butonu
+
+    @FindBy(xpath = "//ul[@class='menu-items scroll-content scroll-scrolly_visible']")
+    public WebElement dashBoardMenu; // Admin Dashboard Menu
+
+    @FindBy(xpath = "//span[normalize-space()='Departments']")
+    public WebElement departmentsButton; // Admin Dashboard Menu >> Departments
+
+    @FindBy(xpath = "//a[@href='https://qa.loyalfriendcare.com/Dashboard/Categories'][normalize-space()='Departments']")
+    public WebElement departmentsDepartmentsButton; // Admin Dashboard >> Departments >> Departments
+
+    @FindBy(xpath = "//a[normalize-space()='Create Departments']")
+    public WebElement createDepartmentsButton; // Admin Dashboard >> Departments >> Create Departments
+
+    @FindBy(xpath = "//span[normalize-space()='Doctors']")
+    public WebElement doctorsButton; // Admin Dashboard >> Doctors
+
+    @FindBy(xpath = "//a[@href='https://qa.loyalfriendcare.com/Dashboard/Clients']")
+    public WebElement doctorsDoctorsButton; // Admin Dashboard >> Doctors >> Doctors
+
+    @FindBy(xpath = "//a[normalize-space()='Create Doctors']")
+    public WebElement createDoctorsButton; // Admin Dashboard >> Doctors >> Create Doctors
+
+    @FindBy(xpath = "//span[@class='title'][normalize-space()='Medicines']")
+    public WebElement medicinesButton; // Admin Dashboard >> Medicines
+
+    @FindBy(xpath = "//a[@href='https://qa.loyalfriendcare.com/Dashboard/Instagrams']")
+    public WebElement medicinesMedicinesButton; // Admin Dashboard >> Medicines >> Medicines
+
+    @FindBy(xpath = "//a[normalize-space()='Create Medicines']")
+    public WebElement createMedicinesButton; // Admin Dashboard >> Medicines >> Create Medicines
+
+    @FindBy(xpath = "//span[normalize-space()='Vaccinations']")
+    public WebElement vaccinationsButton; // Admin Dashboard >> Vaccinations
+
+    @FindBy(xpath = "//span[normalize-space()='Roles']")
+    public WebElement rolesButton; // Admin Dashboard >> Roles
+
+    @FindBy(xpath = "//a[@href='https://qa.loyalfriendcare.com/Dashboard/Roles']")
+    public WebElement rolesRolesButton; // Admin Dashboard >> Roles >> Roles
+
+    @FindBy(xpath = "//a[normalize-space()='Create Role']")
+    public WebElement createRolesButton; // Admin Dashboard >> Roles >> Create Roles
+
+    @FindBy(xpath = "//span[@class='title'][normalize-space()='Users']")
+    public WebElement usersButton; // Admin Dashboard >> Users
+
+    @FindBy(xpath = "//a[@href='https://qa.loyalfriendcare.com/Dashboard/Users']")
+    public WebElement usersUsersButton; // Admin Dashboard >> Users >> Users
+
+    @FindBy(xpath = "//a[normalize-space()='Create User']")
+    public WebElement createUsersButton; // Admin Dashboard >> Users >> Create Users
+
+    @FindBy(xpath = "//span[@class='title'][normalize-space()='Bed managers']")
+    public WebElement bedManagersButton; // Admin Dashboard >> Bed Managers
+
+    @FindBy(xpath = "//a[@href='https://qa.loyalfriendcare.com/Dashboard/Posts']")
+    public WebElement bedManagersBedManagersButton; // Admin Dashboard >> Bed Managers >> Bed Managers
+
+    @FindBy(xpath = "//a[normalize-space()='Create Bed managers']")
+    public WebElement createBedManagersButton; // Admin Dashboard >> Bed Managers >> Create Bed Managers
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/test/java/pages/UserHomePage.java
+++ src/test/java/pages/UserHomePage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -9,5 +11,22 @@ public class UserHomePage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    @FindBy(xpath = "//i[@class='fab fa-facebook-square']")
+    public WebElement facebookLogo; // Homepage/footer
+
+    @FindBy(xpath = "//i[@class='fab fa-twitter-square']")
+    public WebElement twitterLogo; // Homepage/footer
+
+    @FindBy(xpath = "//i[@class='fab fa-youtube-square']")
+    public WebElement youtubeLogo; // Homepage/footer
+
+    @FindBy(xpath = "//i[@class='fab fa-pinterest-square']")
+    public WebElement pinteresLogo; // Homepage/footer
+
+    @FindBy(xpath = "//i[@class='fab fa-instagram']")
+    public WebElement instagramLogo; // Homepage/footer
+
+    @FindBy(xpath = "//div[@class='col-lg-3 col-md-6 col-sm-6']//img[@alt='LoyalFriendCare']")
+    public WebElement loyalFriendCareLogo; // Homepage/footer
 
 }
