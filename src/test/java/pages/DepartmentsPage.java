aea90e9dd0185e8a209c1198637210601b771e97
gkcnyddg <gokcenyedidag@gmail.com>
2024-05-18 13:54:46 +0300

    Departments ve Doctors sayfası locateleri eklendi 18.05.24/gokcen

---
+++ src/test/java/pages/DepartmentsPage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class DepartmentsPage {
+    DepartmentsPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//h1[text()=' Departments']") public WebElement departmentsH1Title;//Departments>yeşil alan h1 başlık;
+    @FindBy(xpath = "//*[@id='filters_col_bt']") public WebElement departmentsListText;//Departments>departments listesi yazısı
+    @FindBy(xpath ="//*[text()='Departments of LoyalFriendCare']") public WebElement departmentsOfLoyalFriendCareText;//Departments>sol taraf departmentsOfLoyalFriendCare yazısı
+    @FindBy(xpath = "//*[text()='Wellness']") public WebElement departmentsListWellnessText;//Departments>sol taraf Wellness yazısı
+    @FindBy(xpath = "//*[text()='Dental Care']") public WebElement departmentsListDentalCareText;//Departments>sol taraf dental Care yazısı
+    @FindBy(xpath = "(//*[text()='Vaccinations'])[3]") public WebElement departmentsListVaccinationsText;//Departments>sol taraf Vaccinations yazısı
+    @FindBy(xpath = "//img[@class='img-fluid']")public WebElement totalDepartmentsImage;//Departments>toplam resim sayısı
+    @FindBy(xpath = "//(img[@class='img-fluid'])[1]")public WebElement wellnessImage;//Departments>wellness resim
+    @FindBy(xpath = "//(img[@class='img-fluid'])[2]")public WebElement dentalCareImage;//Departments> dentalCare resim
+
+
+
+
+
+
+
+}
---
+++ src/test/java/pages/DoctorsPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class DoctorsPage {
+    public DoctorsPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy(xpath = "//h1[text()=' Doctors']") public WebElement pageH1Title;// sayfa yeşil header Doctors baslık
+    @FindBy(xpath = "//img[@class='img-fluid']") public WebElement imageAlejandroM; // Doctors>Doktor resmi
+    @FindBy(xpath = "//input[@id='submit-review']") public WebElement appointmentBookingButton;// Doctors>Appointment booking>Appointment Booking button
+    @FindBy(xpath="//*[@id='filters_col_bt']") public WebElement doctorsListText; // Doctors> Doctors Listesi
+    @FindBy(xpath = "//*[text()='Doctors of LoyalFriendCare']") public WebElement doctorsOfLoyalFriendCareText;// Doctors> Doctors>Doctors Of Loyal Friend Care yazısı
+    @FindBy(xpath = "//*[@class='img-fluid']")public WebElement totalDoctorImage;//Doctors>Sayfadaki toplam doktor sayısı
+    @FindBy(xpath = "//a[text()='Dr. Alejandro Martinez']") public WebElement doctorNameText1; //Doctors>Doktor Alejandro Martinez resmi altında görünen adı
+    @FindBy(xpath = "//a[text()='Dr. Emily Chang']") public WebElement doctorNameText2; //Doctors>Doktor Dr. Emily Chang resmi altında görünen adı
+    @FindBy(xpath = "//p[text()='Profession: Veterinarian ']") public WebElement doctorProfessionTextBox; //Doctors>Doktor Resmi> Profession
+
+    @FindBy(xpath = "//input[@id='Date']" )public WebElement tarihDropdownMenu; //Doctors>Appointment booking>TarihDropdownMenu
+    @FindBy(xpath = "//input[@id='serial']" )public WebElement phoneNumberTextBox;// DoctorsAppointment booking>TarihDropdownMenu>PhoneNumber textbox
+    @FindBy(xpath = "(//*[text()='Wellness'])[2]") public WebElement birimDropDownMenu;// Doctors>Appointment booking>Birim DropdownMenu
+    @FindBy(xpath = "(//*[text()='Dr. Alejandro Martinez'])[3]") public WebElement doktorDropDownMenu;// Doctors>Appointment booking>Doktor adları DropdownMenu
+    @FindBy(xpath = "//textarea[@id='Comment']") public WebElement createMessageTextBox;// Doctors>Appointment booking>Yorum Textboxı
+
+
+
+}
