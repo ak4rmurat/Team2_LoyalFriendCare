fa3a4eff33e830d719cf3ff3459a01a2b0ead0d3
gkcnyddg <gokcenyedidag@gmail.com>
2024-05-18 14:04:13 +0300

    Text ifadeleri label olarak degistirildi

--- src/test/java/pages/DepartmentsPage.java
+++ src/test/java/pages/DepartmentsPage.java
@@ -10,11 +10,11 @@ public class DepartmentsPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
     @FindBy(xpath = "//h1[text()=' Departments']") public WebElement departmentsH1Title;//Departments>yeşil alan h1 başlık;
-    @FindBy(xpath = "//*[@id='filters_col_bt']") public WebElement departmentsListText;//Departments>departments listesi yazısı
-    @FindBy(xpath ="//*[text()='Departments of LoyalFriendCare']") public WebElement departmentsOfLoyalFriendCareText;//Departments>sol taraf departmentsOfLoyalFriendCare yazısı
-    @FindBy(xpath = "//*[text()='Wellness']") public WebElement departmentsListWellnessText;//Departments>sol taraf Wellness yazısı
-    @FindBy(xpath = "//*[text()='Dental Care']") public WebElement departmentsListDentalCareText;//Departments>sol taraf dental Care yazısı
-    @FindBy(xpath = "(//*[text()='Vaccinations'])[3]") public WebElement departmentsListVaccinationsText;//Departments>sol taraf Vaccinations yazısı
+    @FindBy(xpath = "//*[@id='filters_col_bt']") public WebElement departmentsListLabel;//Departments>departments listesi yazısı
+    @FindBy(xpath ="//*[text()='Departments of LoyalFriendCare']") public WebElement departmentsOfLoyalFriendCareLabel;//Departments>sol taraf departmentsOfLoyalFriendCare yazısı
+    @FindBy(xpath = "//*[text()='Wellness']") public WebElement departmentsListWellnessLabel;//Departments>sol taraf Wellness yazısı
+    @FindBy(xpath = "//*[text()='Dental Care']") public WebElement departmentsListDentalCareLabel;//Departments>sol taraf dental Care yazısı
+    @FindBy(xpath = "(//*[text()='Vaccinations'])[3]") public WebElement departmentsListVaccinationsLabel;//Departments>sol taraf Vaccinations yazısı
     @FindBy(xpath = "//img[@class='img-fluid']")public WebElement totalDepartmentsImage;//Departments>toplam resim sayısı
     @FindBy(xpath = "//(img[@class='img-fluid'])[1]")public WebElement wellnessImage;//Departments>wellness resim
     @FindBy(xpath = "//(img[@class='img-fluid'])[2]")public WebElement dentalCareImage;//Departments> dentalCare resim
--- src/test/java/pages/DoctorsPage.java
+++ src/test/java/pages/DoctorsPage.java
@@ -12,11 +12,11 @@ public class DoctorsPage {
     @FindBy(xpath = "//h1[text()=' Doctors']") public WebElement pageH1Title;// sayfa yeşil header Doctors baslık
     @FindBy(xpath = "//img[@class='img-fluid']") public WebElement imageAlejandroM; // Doctors>Doktor resmi
     @FindBy(xpath = "//input[@id='submit-review']") public WebElement appointmentBookingButton;// Doctors>Appointment booking>Appointment Booking button
-    @FindBy(xpath="//*[@id='filters_col_bt']") public WebElement doctorsListText; // Doctors> Doctors Listesi
+    @FindBy(xpath="//*[@id='filters_col_bt']") public WebElement doctorsListLabel; // Doctors> Doctors Listesi
     @FindBy(xpath = "//*[text()='Doctors of LoyalFriendCare']") public WebElement doctorsOfLoyalFriendCareText;// Doctors> Doctors>Doctors Of Loyal Friend Care yazısı
     @FindBy(xpath = "//*[@class='img-fluid']")public WebElement totalDoctorImage;//Doctors>Sayfadaki toplam doktor sayısı
-    @FindBy(xpath = "//a[text()='Dr. Alejandro Martinez']") public WebElement doctorNameText1; //Doctors>Doktor Alejandro Martinez resmi altında görünen adı
-    @FindBy(xpath = "//a[text()='Dr. Emily Chang']") public WebElement doctorNameText2; //Doctors>Doktor Dr. Emily Chang resmi altında görünen adı
+    @FindBy(xpath = "//a[text()='Dr. Alejandro Martinez']") public WebElement doctorName1Label; //Doctors>Doktor Alejandro Martinez resmi altında görünen adı
+    @FindBy(xpath = "//a[text()='Dr. Emily Chang']") public WebElement doctorName2Label; //Doctors>Doktor Dr. Emily Chang resmi altında görünen adı
     @FindBy(xpath = "//p[text()='Profession: Veterinarian ']") public WebElement doctorProfessionTextBox; //Doctors>Doktor Resmi> Profession
 
     @FindBy(xpath = "//input[@id='Date']" )public WebElement tarihDropdownMenu; //Doctors>Appointment booking>TarihDropdownMenu
